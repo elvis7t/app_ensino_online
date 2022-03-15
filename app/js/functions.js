51b3abe333f76507bde4e8515a7f30789518c87a
Elvis Leite <elvis7t@gmail.com>
2022-03-15 13:39:56 -0300

    Versão revisada

--- app/js/functions.js
+++
@@ -1,7 +0,0 @@
-function del(codigo, classe, msg){
-	$("<span></span>").remove();
-    $("<span></span>").html("Deseja excluir "+msg+" "+codigo+"?").appendTo("#msg_conf");
-	$("#confSim").attr("data-reg", codigo);
-	$("#confSim").addClass(classe);
-	$("#confirma").modal("show");
-}
